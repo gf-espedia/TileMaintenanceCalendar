6859c501589c0c7eed93fb485edeb3dad29b43cc
gf-espedia <giulia.favaro@espedia.it>
2019-07-18 13:51:02 +0000

    Update in order to work in any portal

--- webapp/controller/TileMaintenanceCalendarView1.controller.js
+++ webapp/controller/TileMaintenanceCalendarView1.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("TileMaintenanceCalendar.TileMaintenanceCalendar.controller.TileMaintenanceCalendarView1", {
 		onInit: function () {
-			window.location.replace("https://flpnwc-m9a44f3468.dispatcher.hana.ondemand.com/sites/espediasimplemaintenance#Shell-home ");
+			history.back();
 			window.open("https://equipmentcalendar-m9a44f3468.dispatcher.hana.ondemand.com", '_blank');
 		}
 	});
